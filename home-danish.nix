949270e166ad72a5c197c98e3aae7be6b1f9b701
Danish <193341369+daquaipr@users.noreply.github.com>
2026-06-02 21:30:30 +0700

    Update NixOS

--- home-danish.nix
+++ home-danish.nix
@@ -19,6 +19,9 @@
   services = {
     flatpak = {
       enable = true;
+      remotes = [{
+        name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+      }];
       packages = [
         "org.vinegarhq.Sober"
       ];
